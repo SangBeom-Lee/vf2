9f6ccc8f0284e2e6f8fc85bbd70cf918266d97e3
SangBeom-Lee <bumeeya2@naver.com>
2022-03-05 21:01:14 +0900

    푸터 및 메뉴 분리작업

--- src/App.vue
+++ src/App.vue
@@ -5,28 +5,35 @@
       color="primary"
       dark
     >
-    <v-app-bar-nav-icon></v-app-bar-nav-icon>
-    <v-toolbar-title>Page title</v-toolbar-title>
-    <v-spacer/>
-    <v-btn icon to="/about">
-      <v-icon>mdi-magnify</v-icon>
-    </v-btn>
+      <v-app-bar-nav-icon @click="drawer = !drawer"/>
+      <site-title :title="title"></site-title>
     </v-app-bar>
+    <v-navigation-drawer app v-model="drawer">
+      <site-menu></site-menu>
+    </v-navigation-drawer>
 
     <v-content>
       <router-view/>
     </v-content>
+    <site-footer :footer="footer"></site-footer>
 
-    <v-footer app color="primary" dark absolute>
-      <v-spacer/>
-      <div>&copy; {{ new Date().getFullYear() }}</div>
-    </v-footer>
   </v-app>
 </template>
 
 <script>
+import SiteTitle from '@/views/site/TitleView'
+import SiteFooter from '@/views/site/FooterView'
+import SiteMenu from '@/views/site/MenuView'
 
 export default {
-  name: 'App'
+  components: { SiteTitle, SiteFooter, SiteMenu },
+  name: 'App',
+  data () {
+    return {
+      drawer: false,
+      title: '나의 타이틀입니다.',
+      footer: '푸터입니다.'
+    }
+  }
 }
 </script>
---
+++ src/views/site/MenuView.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <v-list-item>
+      <v-list-item-content>
+        <v-list-item-title class='title'>
+          Application
+        </v-list-item-title>
+        <v-list-item-subtitle>
+          subtext
+        </v-list-item-subtitle>
+      </v-list-item-content>
+    </v-list-item>
+    <v-divider></v-divider>
+    <v-list>
+      <v-list-group
+        v-for="(item, i) in items"
+        :key="i"
+        v-model="item.active"
+        :prepend-icon="item.icon"
+        no-action
+      >
+        <template v-slot:activator>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title"></v-list-item-title>
+          </v-list-item-content>
+        </template>
+
+        <v-list-item
+          v-for="subitem in item.subitems"
+          :key="subitem.title"
+          :to="subitem.to"
+        >
+          <v-list-item-content>
+            <v-list-item-title v-text="subitem.title"></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-group>
+    </v-list>
+  </div>
+</template>
+<script>
+export default {
+  props: ['footer'],
+  data () {
+    return {
+      items: [
+        {
+          title: 'Home',
+          icon: 'mdi-home',
+          active: true,
+          subitems: [
+            {
+              title: 'Dashboard',
+              to: '/'
+            },
+            {
+              title: 'About',
+              to: '/about'
+            }
+          ]
+        }
+      ]
+    }
+  }
+}
+</script>
