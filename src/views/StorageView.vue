1ad62b365f7edd21d49adc279953d94c21e840a5
SangBeom-Lee <bumeeya2@naver.com>
2022-03-21 21:29:43 +0900

    firebase storage 파일 다운로드 처리

--- src/views/StorageView.vue
+++ src/views/StorageView.vue
@@ -1,5 +1,8 @@
 <template>
   <v-sheet>
+      <v-textarea v-model="text">
+
+      </v-textarea>
       <v-btn type="primary" width="200px">
             이미지 업로드
             <input
@@ -9,18 +12,21 @@
               @change="getImgPath"
             />
           </v-btn>
+      <v-btn @click="read">read</v-btn>
       <v-btn @click="write">write</v-btn>
   </v-sheet>
 </template>
 <script>
-import { getStorage, ref, uploadBytes } from 'firebase/storage'
+import { getStorage, ref, uploadBytes, getDownloadURL } from 'firebase/storage'
+import axios from 'axios'
 
 export default {
   data () {
     return {
       text: '',
       imgPath: '',
-      imgName: ''
+      imgName: '',
+      url: ''
     }
   },
   methods: {
@@ -28,14 +34,22 @@ export default {
       const storage = getStorage()
       const mountainImagesRef = ref(storage, this.imgName)
 
-      uploadBytes(mountainImagesRef, this.imgpath).then((snapshot) => {
+      await uploadBytes(mountainImagesRef, this.imgpath).then((snapshot) => {
+        console.log(snapshot)
         console.log('Uploaded a blob or file!')
       })
+
+      this.url = await getDownloadURL(mountainImagesRef)
     },
     getImgPath (e) {
       const img = e.target.files[0]
       this.imgpath = img
       this.imgName = img.name.split('.')[0]
+    },
+    async read () {
+      const r = await axios.get(this.url)
+      this.text = r.data
+      console.log(r)
     }
   }
 }
